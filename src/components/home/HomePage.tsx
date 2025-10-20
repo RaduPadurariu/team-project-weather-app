0a266f4758f25718ad4825e81ef7b697d7883bc7
RaduPadurariu <padurariuionutradu@gmail.com>
2025-10-20 15:01:22 +0300

    Implement folder-structure

---
+++ src/components/home/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "./Hero/Hero";
+import HomeApp from "./HomeApp/HomeApp";
+import HomeNews from "./HomeNews/HomeNews";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Hero />
+      <HomeApp />
+      <HomeNews />
+    </div>
+  );
+};
+
+export default HomePage;
