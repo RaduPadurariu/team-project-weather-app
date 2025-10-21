839b575a39976086c92a9885e1da70010c186bff
RaduPadurariu <padurariuionutradu@gmail.com>
2025-10-21 12:42:24 +0300

    Implement footer

--- src/data/data.ts
+++ src/data/data.ts
@@ -4,3 +4,27 @@ export const navbarLinks = [
   { id: 3, title: "news", link: "/news" },
   { id: 4, title: "about", link: "/about" },
 ];
+
+export const raduSocialLinks = [
+  {
+    id: 1,
+    icon: "fa-brands fa-linkedin",
+    src: "https://www.linkedin.com/in/radu-padurariu-13688991/",
+  },
+  { id: 2, icon: "fab fa-github", src: "https://github.com/radupadurariu" },
+  // { id: 3, icon: "fas fa-briefcase", src: "https://radupadurariu.netlify.app" },
+];
+
+export const mariusSocialLinks = [
+  {
+    id: 1,
+    icon: "fa-brands fa-linkedin",
+    src: "https://www.linkedin.com/in/maftei-marius-vasile-58754b116/",
+  },
+  {
+    id: 2,
+    icon: "fab fa-github",
+    src: "https://github.com/MariusVasileMaftei",
+  },
+  // { id: 3, icon: "fas fa-briefcase", src: "https://radupadurariu.netlify.app" },
+];
